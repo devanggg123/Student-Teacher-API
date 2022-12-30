67ed5805d1fb1cb5b0bdea5e02abed40124a68b8
devanggg123 <sfdc.devang123@gmail.com>
2022-12-31 01:21:51 +0530

    hoja

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -16,23 +16,7 @@ public class Teacher {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public int getNumberOfStudents() {
-        return numberOfStudents;
-    }
 
-    public void setNumberOfStudents(int numberOfStudents) {
-        this.numberOfStudents = numberOfStudents;
-    }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
